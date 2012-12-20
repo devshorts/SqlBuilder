3138a9daf31b31e4baf19da090f440de788e5a13
anton <akropp@gmail.com>
2012-12-20 14:59:58 -0500

    Adding basic transactional support as a computation expression

---
+++ DB.fs
@@ -0,0 +1,88 @@
+﻿module DB
+open System
+open System.IO
+open System.Data.SQLite
+open System.Data 
+open System.Data.Common
+
+type SQLBuilder(conn: DbConnection, createTransaction) = 
+    
+    let getFields (reader: DbDataReader) = 
+        [| for i in 0 .. reader.FieldCount - 1 -> reader.[ i ] |]
+
+    do 
+        conn.Open()
+ 
+    member this.Zero () = ()
+
+    member b.Connection = conn
+ 
+    member b.Bind (expr, rest) =
+        // get a single row
+        use cmd = conn.CreateCommand(CommandText = expr)
+ 
+        use reader = cmd.ExecuteReader(CommandBehavior.SingleRow)
+ 
+        rest (getFields reader)
+ 
+    member b.For (expr, rest) =
+        // get all rows
+        use cmd = conn.CreateCommand(CommandText = expr)
+        use reader = cmd.ExecuteReader()
+        while reader.Read() do
+            rest (getFields reader)
+ 
+    member b.Return expr =
+        expr
+ 
+    member b.Combine (expr1, expr2) =
+       expr1
+       expr2
+ 
+    member b.Delay (expr) =
+        expr ()
+
+
+
+type Transaction(conn:IDbConnection) = 
+    member this.executeQuery query = 
+        use cmd = conn.CreateCommand(CommandText = query)
+        cmd.ExecuteNonQuery() |> ignore
+        true
+                                
+    member this.Bind(value, expr) = 
+        if this.executeQuery value then
+            expr()
+        else 
+            false
+        
+    member this.Return expr = expr
+
+    member this.wrapInTransaction expr = 
+            use trans = conn.BeginTransaction()
+            this.executeQuery @"// custom begin " |> ignore
+            try
+                let success = expr()
+
+                this.executeQuery @"// custom end" |> ignore
+
+                trans.Commit()
+
+                true
+            with 
+                | :? System.Exception as ex -> Console.WriteLine ("an error occurred {0}", ex)
+                                               false
+
+                           
+    member this.Yield item = item
+
+    member this.Run expr = this.wrapInTransaction expr
+
+    member this.Delay expr = fun() -> expr()
+                                
+
+let connection = new SQLiteConnection((sprintf "Data Source = %s" @"C:\DB\db.dat"))
+
+let transaction = new Transaction(connection)
+
+let queryDb = SQLBuilder(connection, false)
---
+++ README.md
@@ -0,0 +1,4 @@
+SqlBuilder
+=======
+
+Original computation expression found from http://cs.hubfs.net/topic/None/58747. I added transaction support
